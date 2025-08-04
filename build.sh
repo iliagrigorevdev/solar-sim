2485451f0ac584263b92452252e3222b48dc9b7d
Ilia Grigorev <ru.igsoft@gmail.com>
2025-08-04 21:33:38 +0300

    refactor(css): Separate CSS from HTML

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ mkdir -p bin
 emcc --bind simulation.cpp renderer.cpp -o bin/simulation.js     -std=c++11     -s FULL_ES2=1     --preload-file shader.vert     --preload-file shader.frag
 
 cp index.html bin/index.html
+cp style.css bin/style.css
 
 python -m http.server --directory bin
